cb8862f4a59011bdca5e4b666f482d2eb412189a
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2023-06-05 23:19:39 +0500

    (fix): issue with axios

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,16 +1,21 @@
-import {useLocalStorage} from "@vueuse/core";
 import Axios from "axios";
-
-const token = useLocalStorage("token");
 
 const axios = Axios.create({
   baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL,
   headers: {
     "X-Requested-With": "XMLHttpRequest",
     Accept: "application/json",
-    Authorization: "Bearer " + token.value,
   },
   withCredentials: true,
+});
+
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axios;
