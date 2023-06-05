78ac91e0fbaa3c41dc50a5007a7e3f9f851bdabc
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2023-06-05 23:39:37 +0500

    (feat): deployment updates

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        port: 3000,
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
-    },
-    plugins: [vue()],
-})
+  },
+  build: {
+    minify: "esbuild",
+    target: "esnext",
+  },
+  plugins: [vue()],
+});
